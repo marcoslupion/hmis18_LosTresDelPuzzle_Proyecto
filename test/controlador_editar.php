c224d33590378d775e512c12ab7e227f07baf025
Marcos Lupion Lorente <marcoslupion25@gmail.com>
2018-06-19 19:32:23 +0200

    Cambios en botones y test

--- test/controlador_editar.php
+++ test/controlador_editar.php
@@ -1,7 +1,9 @@
+<script src="../js/general.js"></script>
 <?php
 include 'modelo_editar.php';
 include 'modelo_eliminar_usuario.php';
 include 'modelo_usuario.php';
+include 'modelo_registro.php';
 //error_reporting(0);
 $usuario = $_POST["user_editar"];
 $usuario_anterior = $_POST["user_rep"];
@@ -11,19 +13,32 @@ $contrasenia2=$_POST["pass2_editar"];
 
   $activo = $_POST["resultado_activo"];
 
-  echo"$activo";
+
 if($usuario !=$usuario_anterior){
     #NO SE COMPRUEBA SI EL USUARIO AL QUE SE LE QUIERE CAMBIAR AL NOMBRE ESTA DISPONIBLE, YA QUE NO PUEDE HABER 2 USUARIOS IGUALES
-  
+   
+$modelo_registro = new modelo_registro();
+
+$resultado = $modelo_registro->comprobar_usuario($usuario);
+if($resultado==0){
+
+  echo"<p id='existe'>El usuario ya existe, el nombre de usuario debe ser unico<p> </br>";
+  echo"<button class='btn btn-primary' value='Volver a inicio' onclick='volver_atras()'>Volver atras</Button>";
+ return;
+}
     //AHORA SE VA A HACER QUE SE ACTUALICE EL USUARIO, CONCRETAMENTE EL NOMBRE JUNTO CON TODOS LOS DEMAS DATOS 
     if($contrasenia=="" || $contrasenia2==""){
       $insertar = new modelo_usuario();
       $insertar->modificar_usuario($usuario,$correo,$usuario_anterior,$activo);
+      echo"<p id='contrasenia_ok'>Se han guardado los datos correctamente<p> </br>";
+      echo"<button class='btn btn-primary' value='Volver atras' onclick='volver_atras()'>Volver atras</Button>";
   
     }else{
       //si la contraseña es igual y se cambia 
       $insertar = new modelo_usuario();
       $insertar->modificar_usuario_contrasenia($usuario,$usuario_anterior,$correo,$contrasenia,$activo);
+      echo"<p id='contrasenia_ok'>Se han guardado los datos correctamente<p> </br>";
+      echo"<button class='btn btn-primary' value='Volver atras' onclick='volver_atras()'>Volver atras</Button>";
     }
 
 }else{
@@ -32,11 +47,16 @@ if($usuario !=$usuario_anterior){
   if($contrasenia=="" || $contrasenia2==""){
     $insertar = new modelo_usuario();
     $insertar->modificar_correo($usuario,$correo,$activo);
+    echo"<p id='contrasenia_ok'>Se han guardado los datos correctamente<p> </br>";
+    echo"<button class='btn btn-primary' value='Volver atras' onclick='volver_atras()'>Volver atras</Button>";
 
   }else{
     //si la contraseña es igual y se cambia 
     $insertar = new modelo_usuario();
     $insertar->modificar_contrasenia($usuario,$correo,$contrasenia,$activo);
+    echo"<p id='contrasenia_ok'>Se han guardado los datos correctamente<p> </br>";
+    echo"<button class='btn btn-primary' value='Volver atras' onclick='volver_atras()'>Volver atras</Button>";
+
   }
  
 }
@@ -44,6 +64,3 @@ if($usuario !=$usuario_anterior){
 //$resultado = $editar->editar($usuario,$correo,$contrasenia);
 
 ?>
-<script type="text/javascript">
-  document.location.assign('../vistas/ventana_administrador.php');
-</script>
