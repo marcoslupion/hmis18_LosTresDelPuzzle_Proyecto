2a14d57ef564e3e311a49c78aa97a2c320570669
Marcos Lupion Lorente <marcoslupion25@gmail.com>
2018-06-19 19:43:48 +0200

    Botones unificados

--- test/controlador_crear_usuario.php
+++ test/controlador_crear_usuario.php
@@ -10,7 +10,7 @@ $modelo_registro = new modelo_registro();
 if($user==""| $pass==""| $pass2==""| $email==""){
   echo"<p id='falta_datos'>Debe introducir todos los datos en el formulario<p>";
   echo"</br>";
-  echo"<button class='btn btn-primary' value='Volver a inicio' onclick='volver_atras()'>Volver a inicio</Button>";
+  echo"<button class='btn btn-primary' value='Volver atras' onclick='volver_atras()'>Volver atras</Button>";
 }else{
 
 
@@ -18,7 +18,7 @@ $resultado = $modelo_registro->comprobar_usuario($user);
 if($pass!=$_POST["pass_r2"]){
   echo"<p id='no_coinciden'>Las contraseñas no coinciden<p>";
   echo"</br>";
-  echo"<button class='btn btn-primary' value='Volver a inicio' onclick='volver_atras()'>Volver a inicio</Button>";
+  echo"<button class='btn btn-primary' value='Volver atras' onclick='volver_atras()'>Volver atras</Button>";
 }else{
 
 
@@ -26,12 +26,12 @@ if($resultado==0){
 
   echo"<p id='user_Existe'>El usuario ya existe<p>";
   echo"</br>";
-  echo"<button class='btn btn-primary' value='Volver a inicio' onclick='volver_atras()'>Volver a inicio</Button>";
+  echo"<button class='btn btn-primary' value='Volver atras' onclick='volver_atras()'>Volver atras</Button>";
 }else{
   $modelo_registro->crear_usuario_admin($user,$pass,$email);
   echo"<p id='creado'>El usuario ha sido creado con exito<p>";
   echo"</br>";
-  echo"<button class='btn btn-primary' value='Volver a inicio' onclick='volver_atras()'>Volver a inicio</Button>";
+  echo"<button class='btn btn-primary' value='Volver atras' onclick='volver_atras()'>Volver atras</Button>";
 }
 
 }
