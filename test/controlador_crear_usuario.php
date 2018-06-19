c5655fc8386f1524eb50a191e117874443c9c1f3
Marcos Lupion Lorente <marcoslupion25@gmail.com>
2018-06-19 12:55:23 +0200

    Cambiados label

--- test/controlador_crear_usuario.php
+++ test/controlador_crear_usuario.php
@@ -8,25 +8,25 @@ $email=$_POST["email_r"];
 $pass2=$_POST["pass_r2"];
 $modelo_registro = new modelo_registro();
 if($user==""| $pass==""| $pass2==""| $email==""){
-  echo"<label>Debe introducir todos los datos en el formulario.</br>";
+  echo"<label>Debe introducir todos los datos en el formulario.<label></br>";
   echo"<button class='btn btn-primary' value='Volver a inicio' onclick='volver_atras()'>Volver a inicio</Button>";
 }else{
 
 
 $resultado = $modelo_registro->comprobar_usuario($user);
 if($pass!=$_POST["pass_r2"]){
-  echo"<label>Las contraseñas no coinciden</br>";
+  echo"<label>Las contraseñas no coinciden<label></br>";
   echo"<button class='btn btn-primary' value='Volver atras' onclick='volver_atras()'>Volver a inicio</Button>";
 }else{
 
 
 if($resultado==0){
 
-  echo"<label>El usuario ya existe</br>";
+  echo"<label>El usuario ya existe<label></br>";
   echo"<button class='btn btn-primary' value='Volver atras' onclick='volver_atras()'>Volver a inicio</Button>";
 }else{
   $modelo_registro->crear_usuario_admin($user,$pass,$email);
-  echo"<label>El usuario ha sido creado con exito</br>";
+  echo"<label>El usuario ha sido creado con exito<label></br>";
   echo"<button class='btn btn-primary' value='Volver atras' onclick='volver_atras()'>Volver a inicio</Button>";
 }
 
--- test/controlador_registro.php
+++ test/controlador_registro.php
@@ -11,11 +11,11 @@ $modelo_registro = new modelo_registro();
 $resultado = $modelo_registro->comprobar_usuario($user);
 if($resultado==0){
 
-  echo"<label>El usuario ya existe, vuelva a registrarse con un nombre de usuario diferente </br>";
+  echo"<label>El usuario ya existe, vuelva a registrarse con un nombre de usuario diferente<label> </br>";
   echo"<button class='btn btn-primary' value='Volver a inicio' onclick='volver_a_inicio()'>Volver a inicio de sesion</Button>";
 }else{
   $modelo_registro->crear_usuario($user,$pass,$email);
-  echo"<label>El usuario ha sido creado con exito, vuelve a inicio para iniciar sesión</br>";
+  echo"<label>El usuario ha sido creado con exito, vuelve a inicio para iniciar sesión<label></br>";
   echo"<button class='btn btn-primary' value='Volver a inicio' onclick='volver_a_inicio()'>Volver a inicio de sesion</Button>";
 }
 
