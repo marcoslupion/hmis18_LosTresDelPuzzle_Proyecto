221d886577b92e8fae51ecb20b5181cca4c42768
Alexander Bulychev <ab946@inlumine.ual.es>
2018-06-20 13:57:49 +0200

    Probando ignore coverage

--- test/controlador_comprobar_login.php
+++ test/controlador_comprobar_login.php
@@ -1,11 +1,12 @@
 <?php
-/**
- * @codeCoverageIgnore
- */
+
+// @codeCoverageIgnoreStart
+
 @session_start();
 if(!isset($_SESSION["no_admin"]))
   {
     $script = "<script>document.location.assign('../');</script>";
     echo $script;
   }
+  // @codeCoverageIgnoreEnd
  ?>
